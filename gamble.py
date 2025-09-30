0028b7693f6a8e7e9a1f98c4262df362f2513c08
Sven_Feikens <s.feikens@student.alfa-college.nlgit config --global user.email s.feikens@student.alfa-college.nl>
2025-09-30 13:47:21 +0200

    commit

--- gamble.py
+++ gamble.py
@@ -1,14 +1,14 @@
-dobbel=0
+dobbel = 0
 import random
-guess=690
-while (dobbel!=guess):
-    dobbel=random.randrange(1, 6)
+guess = 690
+while (dobbel != guess):
+    dobbel = random.randrange(1, 6)
     print("1 tot 6, give me your best shot")
-    guess=int(input())
-    if (guess==dobbel):
+    guess = int(input())
+    if (guess == dobbel):
         print("you win")
     else:
-        if (guess != 1 < 7):
-            print ("fuck you")
+        if guess >= 7 or guess <= 0:
+            print("fuck you")
         else:
             print("you lose, the number was", dobbel)
